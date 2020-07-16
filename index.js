404f8d2b5a55a5b13ecadb08e07d0a97265f13bc
Sushant vaidya <pranavvaidya3@gmail.com>
2020-07-16 16:34:48 +0530

    On branch master

--- index.js
+++ index.js
@@ -108,5 +108,5 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () =>
-  console.log(`I have created website and its running on the ${PORT}`)
+  console.log(`I have created APP and its running on the ${PORT}`)
 );
